03c60f65dc88637343b44682c67376769f78296c
jettiy <wnstjr3570@gmail.com>
2026-04-07 21:09:43 +0900

    chore: remove temp python scripts

--- _cleanup.py
+++
@@ -1,7 +0,0 @@
-import os, glob
-d = r'C:\Users\USER-PC\Desktop\onlyAI'
-for f in glob.glob(os.path.join(d, '_*.py')) + glob.glob(os.path.join(d, '_*.txt')):
-    try:
-        os.remove(f)
-        print(f'removed {f}')
-    except: pass
--- _fix_close.py
+++
@@ -1,9 +0,0 @@
-p = r'C:\Users\USER-PC\Desktop\onlyAI\src\pages\explore\ExploreTimeline.tsx'
-with open(p, 'r', encoding='utf-8') as f:
-    c = f.read()
-old = '  },\n\n\nconst FILTER_OPTIONS'
-new = '  },\n];\n\nconst FILTER_OPTIONS'
-c = c.replace(old, new, 1)
-with open(p, 'w', encoding='utf-8') as f:
-    f.write(c)
-print('fixed')
